82708df6edd5750784c73e1b979635214cb16413
Cleyto Ferrari <cleyton@w7br.com>
2014-01-14 11:19:06 -0400

    Projeto inicial

---
+++ ExemploStringExtensions/ExemploStringExtensions/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+
+namespace ExemploStringExtensions
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("".IsNullOrEmpty());//True
+            Console.WriteLine("".IsNotNullOrEmpty());//False
+            Console.WriteLine("{0} {1}".FormatWith("Cleyton","Ferrari"));//Cleyton Ferrari
+            Console.WriteLine("".NullIfEmpty());//
+            Console.WriteLine("Você é Cleyton Ferrari".ToSlug(16));//voce-e-cleyton-f
+            Console.WriteLine("Cleyton Ferrari/Criando metodos".ToSlugWithSegments());//cleyton-ferrari/criando-metodos
+            Console.WriteLine("CleytonFerrari".SeparatePascalCase());//Cleyton Ferrari
+            Console.WriteLine("Cleyton Ferrari".SplitAndTrim(' ').First());//Cleyton
+            Console.WriteLine("Cleyton Ferrari".Contains("Ferrari"));//True
+            Console.WriteLine("Cleyton".Limit(5));//Cleyt
+
+        }
+    }
+}
